c43022a19297e9f854b9fb69d8663b179294d191
Mikhail Titov <89531276499nokia@gmail.com>
2021-11-25 12:41:17 +0300

    ready!

--- app.py
+++ app.py
@@ -1,13 +1,46 @@
 import os
 import shutil
 
-position = ['front', 'side', 'back', 'full']
+
+position = ['front', 'side', 'back', 'full', 'additional']
 sex = ['MEN', 'WOMEN']
 
-count = 1
-for adress, folders, files in os.walk(os.path.join('img', sex[0])):
-    for file in range(len(files)):
-        if position[0] in files[file]:
-            os.rename(os.path.join(adress, files[file]), os.path.join(adress, (f'{str(position[0])}_{str(count)}')))
-            count += 1
 
+def Rename(sex, position):
+    count = 1
+    for adress, folders, files in os.walk(os.path.join('img', sex)):
+        for file in files:
+            if position in file:
+                os.rename(os.path.join(adress, file), os.path.join(adress, f'{str(position)}_{str(count)}.jpg'))
+                count += 1
+
+
+def Show(sex, position):
+    for adress, folders, files in os.walk(os.path.join('img', sex)):
+        for file in files:
+            if position in file:
+                print(os.path.join(adress, file))
+
+
+def Rename_RUN():
+    for s in range(len(sex)):
+        for p in range(len(position)):
+            Rename(sex[s], position[p])
+    print('all is ready >> Rename!')
+
+def Move(sex, position):
+    path = r'C:\Users\Ryzen 7\Desktop\Task_parsing\Base'
+
+    for adress, folders, files in os.walk(os.path.join('img', sex)):
+        for file in files:
+            if position in file:
+                os.replace(os.path.join(adress, file), os.path.join(path, f'{sex}\\{position}\\{file}.jpg'))
+
+def Move_RUN():
+    for s in range(len(sex)):
+        for p in range(len(position)):
+            Move(sex[s], position[p])
+    print('all is ready >> Move!')
+
+Rename_RUN()
+Move_RUN()
